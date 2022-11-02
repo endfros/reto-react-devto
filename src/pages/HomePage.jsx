a6f8172aaa93dcaaa4e051339b03552e78d7faef
Rodrigo Montoya <montoyar32@gmail.com>
2022-11-01 22:57:23 -0500

    Responsiveness added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,58 +1,204 @@
 import React from "react";
+import "./homePage.scss";
 import { ReactComponent as HomeIcon } from "../assets/home.svg";
 import profilePicture from "../assets/pp.webp";
+import { DefaultLayout } from "../layout/DefaultLayout";
+
+import { ReactComponent as SearchIcon } from "../assets/search.svg";
+import { ReactComponent as BellIcon } from "../assets/bell.svg";
+import { ReactComponent as HamburgerIcon } from "../assets/hamburger.svg"
+import { ReactComponent as CloseIcon } from "../assets/closeIcon.svg"
+import { ReactComponent as ReadingIcon } from "../assets/readingIcon.svg"
+import { ReactComponent as ListingsIcon } from "../assets/listingsIcon.svg"
+import { ReactComponent as PodcastsIcon } from "../assets/podcastsIcon.svg"
+import { ReactComponent as VideosIcon } from "../assets/videosIcon.svg"
+import { ReactComponent as TagsIcon } from "../assets/tagsIcon.svg"
+import { ReactComponent as FaqIcon } from "../assets/faqIcon.svg"
+import { ReactComponent as ShopIcon } from "../assets/shopIcon.svg"
+import { ReactComponent as SponsorsIcon } from "../assets/sponsorsIcon.svg"
+import { ReactComponent as AboutIcon } from "../assets/aboutIcon.svg"
+import { ReactComponent as ContactIcon } from "../assets/contactIcon.svg"
+import { ReactComponent as GuidesIcon } from "../assets/guidesIcon.svg"
+import { ReactComponent as ComparisonsIcon } from "../assets/comparisonsIcon.svg"
+
+import { ReactComponent as ConductIcon } from "../assets/conductIcon.svg"
+import { ReactComponent as PolicyIcon } from "../assets/policyIcon.svg"
+import { ReactComponent as TermsIcon } from "../assets/termsIcon.svg"
+
+
+import { ReactComponent as TwitterIcon } from "../assets/twitterIcon.svg"
+import { ReactComponent as FacebookIcon } from "../assets/facebookIcon.svg"
+import { ReactComponent as GithubIcon } from "../assets/githubIcon.svg"
+import { ReactComponent as InstagramIcon } from "../assets/instagramIcon.svg"
+import { ReactComponent as TwitchIcon } from "../assets/twitchIcon.svg"
+
+import { ReactComponent as ConfigIcon } from "../assets/configIcon.svg"
 
 import "../App.scss";
 
 export const HomePage = () => {
   return (
-    <>
+    <DefaultLayout>
       <section className="layout">
         <aside className="navbars">
           <nav className="navbar">
             <ul className="navbar__list">
-              <li className="navbar__list-item">
-                <div className="navbar__navigation-item">
-                  <HomeIcon />
-                  Home
-                </div>
-              </li>
-              <li className="navbar__list-item">
-                <div className="navbar__navigation-item">
-                  <HomeIcon />
-                  Home
-                </div>
-              </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <HomeIcon />
+                    Home
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <ReadingIcon />
+                    Reading List
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <ListingsIcon />
+                    Listings
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <PodcastsIcon />
+                    Podcasts
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <VideosIcon />
+                    Videos
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <TagsIcon />
+                    Tags
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <FaqIcon />
+                    FAQ
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <ShopIcon />
+                    Forem Shop
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <SponsorsIcon />
+                    Sponsors
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <AboutIcon />
+                    About
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <ContactIcon />
+                    Contact
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <GuidesIcon />
+                    Guides
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <ComparisonsIcon />
+                    Software comparisons
+                  </div>
+                </li>
             </ul>
           </nav>
           <nav className="navbar">
             <p className="navbar__title">Other</p>
             <ul className="navbar__list">
-              <li className="navbar__list-item">
-                <div className="navbar__navigation-item">
-                  <HomeIcon />
-                  Home
-                </div>
-              </li>
-              <li className="navbar__list-item">
-                <div className="navbar__navigation-item">
-                  <HomeIcon />
-                  Home
-                </div>
-              </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <ConductIcon />
+                    Code of Conduct
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <PolicyIcon />
+                    Privacy Policy
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <TermsIcon />
+                    Terms of Use
+                  </div>
+                </li>
             </ul>
           </nav>
+          <ul className="navbar__social-icons">
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <TwitterIcon />
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <FacebookIcon />
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <GithubIcon />
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <InstagramIcon />
+                  </div>
+                </li>
+                <li className="navbar__list-item">
+                  <div className="navbar__navigation-item">
+                    <TwitchIcon />
+                  </div>
+                </li>
+          </ul>
           <nav className="navbar">
-            <p className="navbar__title">My Tags</p>
+            <header className="navbar__tags-header">
+              <p className="navbar__title">My Tags</p>
+              <button className="navbar__list-item" ><ConfigIcon/></button>
+            </header>
             <ul className="navbar__list">
               <li className="navbar__list-item">
                 <div className="navbar__navigation-item">#webdev</div>
               </li>
               <li className="navbar__list-item">
-                <div className="navbar__navigation-item">#webdev</div>
+                <div className="navbar__navigation-item">#career</div>
               </li>
               <li className="navbar__list-item">
-                <div className="navbar__navigation-item">#webdev</div>
+                <div className="navbar__navigation-item">#todayilearned</div>
+              </li>
+              <li className="navbar__list-item">
+                <div className="navbar__navigation-item">#github</div>
+              </li>
+              <li className="navbar__list-item">
+                <div className="navbar__navigation-item">#discuss</div>
+              </li>
+              <li className="navbar__list-item">
+                <div className="navbar__navigation-item">#weeklyretro</div>
+              </li>
+              <li className="navbar__list-item">
+                <div className="navbar__navigation-item">#weeklylearn</div>
               </li>
             </ul>
           </nav>
@@ -322,6 +468,6 @@ export const HomePage = () => {
           </section>
         </aside>
       </section>
-    </>
+    </DefaultLayout>
   );
 };
