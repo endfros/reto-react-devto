0fe00bbaf91973a2a5209c684727f9563425906b
Hector <Hector.bprz@gmail.com>
2022-11-02 23:24:42 -0500

    feature profile Page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { PostPage } from "./pages/PostPage";
-// import { Navbar } from "./components/Navbar";
-import { DefaultLayout } from "./layout/DefaultLayout";
 
 function App() {
+  const [token, setToken] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/user/me" element={<ProfilePage />} />
+        <Route path="/user/me" element={<ProfilePage token={token} />} />
         <Route path="/post" element={<PostPage />} />
-        <Route path="/navbar" element={<DefaultLayout />} />
+        <Route path="/login" element={<loginPage setToken={setToken} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "../components/Navbar";
 export const DefaultLayout = (props) => {
   return (
     <>
-      <Navbar />;
+      <Navbar />
       <main>
         <div>{props.children}</div>
       </main>
